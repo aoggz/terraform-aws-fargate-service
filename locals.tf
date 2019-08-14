a7caa17899908aea918587f06c597836ef06b764
Andrew Ogburn <aogburn@zoll.com>
2019-08-14 08:09:25 -0400

    fix: Update reference to listener ARN

--- locals.tf
+++ locals.tf
@@ -3,5 +3,5 @@ locals {
   alb_subnets      = "${split(",", var.alb_internal == "1" ? join(",", var.alb_subnets_private) : join(",", var.alb_subnets_public))}"
   datadog_cpu      = "${var.datadog_enabled == "1" ? var.datadog_cpu : "0"}"
   datadog_memory   = "${var.datadog_enabled == "1" ? var.datadog_memory : "0"}"
-  alb_listener_arn = "${var.alb_listener_default_action == "forward" ? aws_lb_listener.front_end_forward.arn : aws_lb_listener.front_end_redirect.arn}"
+  alb_listener_arn = "${var.alb_listener_default_action == "forward" ? aws_lb_listener.front_end_forward.0.arn : aws_lb_listener.front_end_redirect.0.arn}"
 }
