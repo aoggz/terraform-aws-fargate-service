b95971ffa2be738ea079953a048b0c430018695a
Andrew Ogburn <aogburn@zoll.com>
2019-08-20 09:12:19 -0400

    fix: Update ternary to allow internal ALB

--- locals.tf
+++ locals.tf
@@ -1,9 +1,6 @@
 locals {
   # Had to do it a complicated way: https://github.com/hashicorp/terraform/issues/18259#issuecomment-438407005
-  alb_subnets = split(
-    ",",
-    var.alb_internal == "1" ? join(",", var.alb_subnets_private) : join(",", var.alb_subnets_public),
-  )
+  alb_subnets           = var.alb_internal ? var.alb_subnets_private : var.alb_subnets_public
   container_definitions = <<-EOT
   [
     {
